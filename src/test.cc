4bf0f6f23241d8536293ffac41c9eb56542b84d1
Minaev Mike <minaev.mike@gmail.com>
2014-09-29 00:36:11 +0400

    fork

--- src/test.cc
+++ src/test.cc
@@ -5,6 +5,7 @@
 #include <boost/lockfree/queue.hpp>
 #define PORT 8080
 
+#include <sys/ioctl.h>
 
 void listener_cb(struct evconnlistener *, evutil_socket_t, struct sockaddr *, int socklen, void *);
 void conn_writecb(struct bufferevent *, void *);
@@ -33,42 +34,105 @@ static void threadFunc1() {
 		}
 	}
 }
+void set_nonblock(int fd) {
+    int r;
+    int set;
+
+    set = 1;
+
+    do
+        r = ioctl(fd, FIONBIO, &set);
+    while (r == -1 && errno == EINTR);
+
+    assert(r == 0);
+}
+
 volatile bool isRun = true;
 int main()
 {
-    struct event_base *base;
-    struct evconnlistener *listener;
-    struct sockaddr_in sin;
-	int numThreads = 2 * std::thread::hardware_concurrency();
-    base = event_base_new();
-    if (!base) {
-        fprintf(stderr, "Could not initialize libevent!\n");
-        return 1;
+
+    int child_cnt;
+    int port;
+    int i;
+    int r;
+    int fd;
+    struct sockaddr_in addr;
+
+    child_cnt = 4;
+    port = 8000;
+    fprintf(stdout, "[%d] master started\n", getpid());
+
+    /* Create and bind server socket */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(fd != -1);
+    r = 1;
+    r = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &r, sizeof(r));
+    assert(r == 0);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    r = bind(fd, (struct sockaddr*) &addr, sizeof(addr));
+    assert(r == 0);
+
+    /* Start accepting
+     * connections */
+    r = listen(fd, 1024);
+    assert(r == 0);
+    set_nonblock(fd);
+    int pid;
+    for(int j = 0; j < 4;++j) {
+        pid = fork();
+        if(pid == 0) {
+            event_base *base = event_base_new();
+            while (1) {
+                int pport = accept(fd, (struct sockaddr*)NULL, NULL); 
+                if (pport != -1) {
+                    printf("pport: %d\n", pport);
+                    listener_cb(NULL, pport, NULL, 0, (void*)base);
+                    //Child
+                    /*struct event_base *base;
+                    struct evconnlistener *listener;
+                    struct sockaddr_in sin;
+                    int numThreads = std::thread::hardware_concurrency();
+                    base = event_base_new();
+                    if (!base) {
+                        fprintf(stderr, "Could not initialize libevent!\n");
+                        return 1;
+                    }
+
+                    memset(&sin, 0, sizeof(sin));
+                    sin.sin_port = htons(pport);
+                    sin.sin_family = AF_INET;
+
+                    listener = evconnlistener_new_bind(base, listener_cb_1, (void *)base,
+                            LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
+                            (struct sockaddr*)&sin,
+                            sizeof(sin));
+                    printf("Starting server on localhost:%d\nNumber of working threads: %d\n", pport, numThreads);
+                    for(int i = 0; i < numThreads; ++i) {
+                        std::thread (threadFunc1).detach();
+                    }
+                    if (!listener) {
+                        fprintf(stderr, "Could not create a listener!\n");
+                        return 1;
+                    }
+                    evconnlistener_set_error_cb(listener, accept_error_cb);
+
+                    event_base_dispatch(base);
+
+                    evconnlistener_free(listener);
+                    event_base_free(base);
+                    printf("done\n");*/
+                }
+                usleep(0);
+            }
+        }
     }
-
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_port = htons(PORT);
-    sin.sin_family = AF_INET;
-
-    listener = evconnlistener_new_bind(base, listener_cb_1, (void *)base,
-        LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
-        (struct sockaddr*)&sin,
-        sizeof(sin));
-	printf("Starting server on localhost:%d\nNumber of working threads: %d\n", PORT, numThreads);
-	for(int i = 0; i < numThreads; ++i) {
-		std::thread (threadFunc1).detach();
-	}
-    if (!listener) {
-        fprintf(stderr, "Could not create a listener!\n");
-        return 1;
+    while(1){ 
+        usleep(0);
     }
-    evconnlistener_set_error_cb(listener, accept_error_cb);
-
-    event_base_dispatch(base);
-
-    evconnlistener_free(listener);
-    event_base_free(base);
-    printf("done\n");
     return 0;
 }
 
